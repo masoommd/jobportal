ffa590b7e793e5f45b282dae5aca077bf7a37d4d
Md Masoom <mdmasoom9060@gmail.com>
2025-02-23 04:32:15 +0530

    Changes URL

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOption = {
-    origin:'http://localhost:5173',
+    origin:'https://jobportal-2wks.onrender.com',
     credentials:true
 }
 app.use(cors(corsOption))
